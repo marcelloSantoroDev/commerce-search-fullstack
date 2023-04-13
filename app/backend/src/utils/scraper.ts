510d20a3e0a27d09c43ac7fb8342bbe8cacacdb2
Marcello <marcelloaltermundi@gmail.com>
2023-04-13 20:38:46 -0300

    finishes day 3

--- app/backend/src/utils/scraper.ts
+++ app/backend/src/utils/scraper.ts
@@ -1,51 +1,11 @@
-// const puppeteer = require('puppeteer');
-// const fs = require('fs');
-// const { log } = require('console');
-
-// (async (url) => {
-//   const browser = await puppeteer.launch({ headless: false });
-
-//   const page = await browser.newPage();
-
-//   await page.goto(url);
-  
-//   const data = await page.evaluate(() => {
-//     const cards = Array.from(document.querySelectorAll('.Paper_Paper__HIHv0'));
-
-//     const results = cards.map((card) => {
-//       const link = card.querySelector('a').href;
-//       const image = card.querySelector('.SearchCard_ProductCard_Body__2wM_H > div > span > img').src;
-//       const title = card.querySelector('.SearchCard_ProductCard_NameWrapper__Gv0x_ > div > h2').innerText;
-//       const price = card.querySelector('.SearchCard_ProductCard_NameWrapper__Gv0x_ + div > p').innerText;
-      
-//       return {
-//         link,
-//         image,
-//         title,
-//         price
-//       };
-//     });
-
-//     return results;
-//   });
-
-// const regex = /^https:\/\/i\.zst\.com\.br\/thumbs\/45\//;
-// const filter = data.filter((product) => regex.test(product.image));
-
-//     fs.writeFile('api.json', JSON.stringify(filter, null, 2), (err) => {
-//     if(err) return log('erro');
-//     log('deu bom')
-//   })
-
-//   await browser.close();
-// })();
-
-import puppeteer from 'puppeteer';
 import fs from 'fs';
 import { log } from 'console';
+import puppeteer from 'puppeteer'
 
 const scraper = async (url: string) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+  executablePath: '/usr/bin/chromium-browser'
+});
 
   const page = await browser.newPage();
 
