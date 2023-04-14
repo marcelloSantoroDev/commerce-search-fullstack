9b7ebada263d5a040ce5bdd23be22b625807a34f
Marcello <marcelloaltermundi@gmail.com>
2023-04-14 11:35:11 -0300

    feat: adds GET methods to make scraping jsons available

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,30 +1,55 @@
 import app from "./app";
+import fs from "fs";
+import path from "path";
 import scraper from "./utils/scraper";
+
 const PORT = 3001;
 
-const server = app.listen(PORT, () => console.log(
-    `Rodando na porta ${PORT}`
-));
-
-const fridgeUrl = 'https://www.buscape.com.br/geladeira'
-const TVUrl = 'https://www.buscape.com.br/tv'
-const mobileUrl = 'https://www.buscape.com.br/celular'
-
-app.get('/api/buscape/celular', async (_req, res) => {
-    await scraper(mobileUrl);
-    const data = await fetch('./api.json')
-
-    return res.status(200).json(data)
+const server = app.listen(PORT, async () => {
+    await Promise.all([
+        scraper('https://www.buscape.com.br/celular'),
+        scraper('https://www.buscape.com.br/geladeira'),
+        scraper('https://www.buscape.com.br/tv')
+    ]);
+    console.log(`rodando na porta ${PORT}`);
 });
 
-// app.get('/api/buscape/geladeira', (_req, res) => {
-//     const data = scraper(fridgeUrl);
-//     return res.status(200).json(data);
-// })
 
-// app.get('/api/buscape/celular', (_req, res) => {
-//     const data = scraper(TVUrl);
-//     return res.status(200).json(data);
-// })
+
+const __dirname = '/home/marcello-sonoro/GITHUB/desafios-tecnicos/lexart-fullstack/lexart-fullstack-test/app/backend/'
+
+
+app.get('/api/buscape/celular', async (_req, res) => {
+   const filePath = path.join(__dirname, 'mobile.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao ler arquivo' });
+        }
+        return res.status(200).json(JSON.parse(data));
+    });
+});
+
+app.get('/api/buscape/geladeira', async (_req, res) => {
+    const filePath = path.join(__dirname, 'fridge.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao ler arquivo' });
+        }
+        return res.status(200).json(JSON.parse(data));
+    });
+});
+
+app.get('/api/buscape/tv', async (_req, res) => {
+    const filePath = path.join(__dirname, 'tv.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao ler arquivo' });
+        }
+        return res.status(200).json(JSON.parse(data));
+    });
+});
 
 export default server;
--- app/backend/src/utils/scraper.ts
+++ app/backend/src/utils/scraper.ts
@@ -3,9 +3,7 @@ import { log } from 'console';
 import puppeteer from 'puppeteer'
 
 const scraper = async (url: string) => {
-  const browser = await puppeteer.launch({
-  executablePath: '/usr/bin/chromium-browser'
-});
+  const browser = await puppeteer.launch();
 
   const page = await browser.newPage();
 
@@ -38,14 +36,78 @@ const scraper = async (url: string) => {
     price: string;
   }
   const regex = /^https:\/\/i\.zst\.com\.br\/thumbs\/45\//;
-const filter = data.filter((product: product) => regex.test(product.image));
+  const filter = data.filter((product: product) => regex.test(product.image));
 
-    fs.writeFile('api.json', JSON.stringify(filter, null, 2), (err) => {
-    if(err) return log('erro');
-    log('deu bom')
-  })
+  let file;
+  if (url === 'https://www.buscape.com.br/geladeira') {
+    file = 'fridge.json';
+  } else if (url === 'https://www.buscape.com.br/tv') {
+    file = 'tv.json';
+  } else if (url === 'https://www.buscape.com.br/celular') {
+    file = 'mobile.json';
+  }
+
+  if (file) {
+    fs.writeFile(file, JSON.stringify(filter, null, 2), (err) => {
+      if (err) return log(`Error: ${err}}`);
+      log('Scraping successful')
+    });
+  }
 
   await browser.close();
-}
+
+};
+
 
 export default scraper;
+
+// import fs from 'fs';
+// import { log } from 'console';
+// import puppeteer from 'puppeteer'
+
+// const url = 'https://www.buscape.com.br/celular';
+
+// (async () => {
+//   const browser = await puppeteer.launch();
+
+//   const page = await browser.newPage();
+
+//   await page.goto(url);
+
+//   const data = await page.evaluate(() => {
+//     const cards = Array.from(document.querySelectorAll('.Paper_Paper__HIHv0'));
+
+//     const results = cards.map((card) => {
+//       const link = (card.querySelector('a') as HTMLAnchorElement).href;
+//       const image = (card.querySelector('.SearchCard_ProductCard_Body__2wM_H > div > span > img') as HTMLImageElement).src;
+//       const title = (card.querySelector('.SearchCard_ProductCard_NameWrapper__Gv0x_ > div > h2') as HTMLHeadingElement).innerText;
+//       const price = (card.querySelector('.SearchCard_ProductCard_NameWrapper__Gv0x_ + div > p') as HTMLParagraphElement).innerText;
+
+//       return {
+//         link,
+//         image,
+//         title,
+//         price
+//       };
+//     });
+
+//     return results;
+//   });
+
+//   interface product {
+//     link: string;
+//     image: string;
+//     title: string;
+//     price: string;
+//   }
+//   const regex = /^https:\/\/i\.zst\.com\.br\/thumbs\/45\//;
+// const filter = data.filter((product: product) => regex.test(product.image));
+
+//     fs.writeFile('api.json', JSON.stringify(filter, null, 2), (err) => {
+//     if(err) return log('erro');
+//     log('deu bom')
+//   })
+
+//   await browser.close();
+// })()
+
