7724b6e8031922e1b19774d9a33669eae35e0ac4
Marcello <marcelloaltermundi@gmail.com>
2023-04-18 15:34:39 -0300

    .

--- app/backend/src/services/searchesService.js
+++ app/backend/src/services/searchesService.js
@@ -2,7 +2,7 @@ const { searches } = require('../database/models')
 const createSearch = async (data) => {
     try {
         await searches.create(data);
-        return { tyṕe: null, message: 'Created!' }
+        return { type: null, message: 'Created!' }
     } catch (error) {
         return { type: error.type, message: error.message }
     }
