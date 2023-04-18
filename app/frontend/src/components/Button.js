ffff2aedcdd08821a2def2b134819fbfa8216278
Marcello <marcelloaltermundi@gmail.com>
2023-04-18 16:08:38 -0300

    fix: changes url for scraping

--- app/frontend/src/components/Button.js
+++ app/frontend/src/components/Button.js
@@ -3,76 +3,76 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import AppContext from '../context/AppContext';
 import meliApi from '../utils/meliApi';
-import dbApi from '../utils/dbApi';
+// import dbApi from '../utils/dbApi';
 import buscapeApi from '../utils/buscapeApi';
-import saveOnDb from '../utils/saveOnDb';
+// import saveOnDb from '../utils/saveOnDb';
 
 function ButtonComp(props) {
   const { type } = props;
-  const { category, api, setApiData, searched, setSearched, saved, setSaved } = useContext(AppContext);
+  const { category, api, setApiData, searched, setSearched } = useContext(AppContext);
 
-  const handleSavedArr = () => {
-    const isCatSaved = saved.category.some((e) => e === category);
-    const isSiteSaved = saved.website.some((e) => e === api);
-    if (isCatSaved && isSiteSaved) {
-      return true
-    } else {
-      setSaved({
-        ...saved,
-        category: [...saved.category, category],
-        website: [...saved.website, api]
-      })
-      return false
-    }
-  }
+  // const handleSavedArr = () => {
+  //   const isCatSaved = saved.category.some((e) => e === category);
+  //   const isSiteSaved = saved.website.some((e) => e === api);
+  //   if (isCatSaved && isSiteSaved) {
+  //     return true
+  //   } else {
+  //     setSaved({
+  //       ...saved,
+  //       category: [...saved.category, category],
+  //       website: [...saved.website, api]
+  //     })
+  //     return false
+  //   }
+  // }
 
   const handleMeliClick = async () => {
-    const isSaved = handleSavedArr();
+    // const isSaved = handleSavedArr();
     if (searched.length === 0) {
-      if (isSaved) {
-        const data = await dbApi({category, api});
-        setApiData(data);
-      } else {
+      // if (isSaved) {
+        // const data = await dbApi({category, api});
+        // setApiData(data);
+      // } else {
         const data = await meliApi(category);
         setApiData(data);
-        saveOnDb(data, 'meli', category);
-      }
+        // saveOnDb(data, 'meli', category);
+      // }
     } else {
-      if (isSaved) {
-        const data = await dbApi({ category, api });
-        setSearched('');
-        setApiData(data);
-      } else {
+      // if (isSaved) {
+      //   const data = await dbApi({ category, api });
+      //   setSearched('');
+      //   setApiData(data);
+      // } else {
         const data = await meliApi(searched);
         setSearched('');
         setApiData(data);
-        await saveOnDb(data, 'meli', category);
-      }
+        // await saveOnDb(data, 'meli', category);
+      // }
     }
   }
 
   const handleBuscapeClick = async () => {
-    const isSaved = handleSavedArr();
+    // const isSaved = handleSavedArr();
     if (searched.length === 0) {
-      if (isSaved) {
-        const data = await dbApi({ category, api });
-        setApiData(data);
-      } else {
+      // if (isSaved) {
+      //   const data = await dbApi({ category, api });
+      //   setApiData(data);
+      // } else {
         const data = await buscapeApi(category);
         setApiData(data);
-        await saveOnDb(data, 'buscape', category)
-      }
+        // await saveOnDb(data, 'buscape', category)
+      // }
     } else {
-      if (isSaved) {
-        const data = await dbApi({ category, api });
-        setSearched('');
-        setApiData(data);
-      } else {
+      // if (isSaved) {
+      //   const data = await dbApi({ category, api });
+      //   setSearched('');
+      //   setApiData(data);
+      // } else {
         const data = await buscapeApi(searched);
         setSearched('');
         setApiData(data);
-        saveOnDb(data, 'buscape', category);
-      }
+        // saveOnDb(data, 'buscape', category);
+      // }
     }
   }
 
--- app/frontend/src/utils/buscapeApi.js
+++ app/frontend/src/utils/buscapeApi.js
@@ -1,13 +1,35 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const buscapeApi = async (query) => {
+//   const url = `http://localhost:3001/api/buscape/${query}`;
+//   try {
+//     const response = await axios.get(url);
+//     return response.data.results;
+//   } catch (error) {
+//     throw new Error(error.message);
+//   }
+// };
+
+// export default buscapeApi;
 
 const buscapeApi = async (query) => {
-  const url = `http://localhost:3001/api/buscape/${query}`;
+
+  const response = { query };
+
   try {
-    const response = await axios.get(url);
-    return response.data.results;
+    const data = await fetch('http://localhost:3001/api/buscape', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(response)
+    });
+
+    return data;
   } catch (error) {
-    throw new Error(error.message);
+    return `Error: ${error}`
   }
+
 };
 
 export default buscapeApi;
