f5f9f1d28374aa979f490949cab96054222e6066
Marcello <marcelloaltermundi@gmail.com>
2023-04-18 17:40:30 -0300

    fix: erases codes in comment

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -1,6 +1,4 @@
 const app = require('./app');
-const fs = require('fs');
-const path = require('path');
 const scraper = require('./utils/scraper');
 
 const PORT = 3001;
@@ -8,44 +6,5 @@ const PORT = 3001;
 const server = app.listen(PORT, async () => {
     console.log(`rodando na porta ${PORT}`);
 });
-
-// const dirname = '/home/marcello-sonoro/GITHUB/desafios-tecnicos/lexart-fullstack/lexart-fullstack-test/app/backend/apis/'
-
-
-// app.get('/api/buscape/celular', async (_req, res) => {
-//    await scraper('https://www.buscape.com.br/celular');
-//    const filePath = path.join(dirname, 'mobile.json');
-//     fs.readFile(filePath, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error(err);
-//             return res.status(500).json({ error: 'Erro ao ler arquivo' });
-//         }
-//         return res.status(200).json(JSON.parse(data));
-//     });
-// });
-
-// app.get('/api/buscape/geladeira', async (_req, res) => {
-//     await scraper('https://www.buscape.com.br/geladeira')
-//     const filePath = path.join(dirname, 'fridge.json');
-//     fs.readFile(filePath, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error(err);
-//             return res.status(500).json({ error: 'Erro ao ler arquivo' });
-//         }
-//         return res.status(200).json(JSON.parse(data));
-//     });
-// });
-
-// app.get('/api/buscape/tv', async (_req, res) => {
-//     await scraper('https://www.buscape.com.br/tv')
-//     const filePath = path.join(dirname, 'tv.json');
-//     fs.readFile(filePath, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error(err);
-//             return res.status(500).json({ error: 'Erro ao ler arquivo' });
-//         }
-//         return res.status(200).json(JSON.parse(data));
-//     });
-// });
 
 module.exports = server;
