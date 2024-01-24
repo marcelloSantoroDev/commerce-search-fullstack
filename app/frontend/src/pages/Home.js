7a2d7e1b93ee4d0c11ab21be56c5cb91a6aa996b
Marcello <marcelloaltermundi@gmail.com>
2024-01-24 11:57:53 -0300

    starts changing requests to the server side

--- app/frontend/src/pages/Home.js
+++ app/frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import SelectComp from '../components/Select'
 import './CSS/Home.css'
 import ButtonComp from '../components/Button'
